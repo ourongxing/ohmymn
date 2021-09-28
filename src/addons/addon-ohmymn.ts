9e7b8a1d226b4987290784caae80553c4226c530
ourongxing <orongxing@gmail.com>
2021-09-28 16:41:38 +0800

    preview 1.1.0 beta

--- src/addons/addon-ohmymn.ts
+++ src/addons/addon-ohmymn.ts
@@ -1,6 +1,6 @@
 const config: IConfig = {
   name: "OhMyMN",
-  intro: "version: 1.0.4 \nmade by @ourongxing",
+  intro: "version: 1.1.0 beta \nmade by @ourongxing",
   link: "https://github.com/ourongxing/ohmymn",
   settings: [
     {
