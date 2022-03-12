1efacda2ad2aeac4a2924fbc8af12061ae12895c
ourongxing <orongxing@gmail.com>
2022-03-12 18:53:35 +0800

    chore: add some commit specifications

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,13 +5,14 @@ module.exports = {
       2,
       "always",
       [
-        "feat", // 新功能（feature）
+        "feat", // 新功能
         "fix", // 修补 bug
-        "docs", // 文档（documentation）
+        "docs", // 文档
+        "note", // 注释或其他文字
         "style", // 格式（不影响代码运行的变动）
-        "refactor", // 重构（即不是新增功能，也不是修改bug的代码变动）
+        "refactor", // 重构（即不是新增功能，也不是修改 bug 的代码变动）
         "test", // 增加测试
-        "pref", // 提高性能
+        "perf", // 提高性能
         "chore", // 构建过程或辅助工具的变动
         "release" // 发布新版本
       ]
