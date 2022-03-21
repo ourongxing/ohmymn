aeb47528ea6a924c81353a54dd00ab31207519ae
Exodus! <49256406+BryanHuang66@users.noreply.github.com>
2022-03-22 00:35:43 +0800

    add notation to MargiNote API

--- src/typings/MarginNote/NoteDatabase.d.ts
+++ src/typings/MarginNote/NoteDatabase.d.ts
@@ -1,114 +1,298 @@
 import { MbBookNote } from "./MbBookNote"
+/**
+ * @property {string} currentTopicId
+ * @property {Date} lastVisit
+ * @property {string} docMd5
+ * @property {string} pathFile
+ * @property {string} docTitle
+ */
 export class MbBook {
+  /**
+   * Current topic ID
+   *
+   * @type {string}
+   * @memberof MbBook
+   */
   readonly currentTopicId?: string
+  /**
+   * Date of last visit
+   *
+   * @type {Date}
+   * @memberof MbBook
+   */
   readonly lastVisit?: Date
+  /**
+   * docMd5 of the book
+   *
+   * @type {string}
+   * @memberof MbBook
+   */
   readonly docMd5?: string
+  /**
+   * pathFile of the book
+   *
+   * @type {string}
+   * @memberof MbBook
+   */
   readonly pathFile?: string
+  /**
+   * Title of the book
+   *
+   * @type {string}
+   * @memberof MbBook
+   */
   readonly docTitle?: string
 }
 
+/**
+ * @property {string} title
+ * @property {string} topicId
+ * @property {Date} lastVisit
+ * @property {string} mainDocMd5
+ * @property {number|boolean} syncMode
+ * @property {string} categoryList
+ * @property {string} hashtags
+ * @property {string} docTitle
+ * @property {DictObj} options
+ * @property {Array<any>} documents
+ * @property {Array<any>} notes
+ * @property {boolean} hideLinksInMindMapNode
+ */
 export class MbTopic {
+  /**
+   *
+   * @type {string}
+   * @memberof MbTopic
+   */
   title?: string
+  /**
+   * 
+   *
+   * @type {string}
+   * @memberof MbTopic
+   */
   readonly topicId?: string
+  /**
+   *
+   *
+   * @type {Date}
+   * @memberof MbTopic
+   */
   readonly lastVisit?: Date
+  /**
+   *
+   *
+   * @type {string}
+   * @memberof MbTopic
+   */
   readonly mainDocMd5?: string
+  /**
+   *
+   *
+   * @type {Date}
+   * @memberof MbTopic
+   */
   readonly historyDate?: Date
+  /**
+   *
+   *
+   * @type {(number | boolean)}
+   * @memberof MbTopic
+   */
   readonly syncMode?: number | boolean
+  /**
+   *
+   *
+   * @type {string}
+   * @memberof MbTopic
+   */
   readonly categoryList?: string
+  /**
+   *
+   *
+   * @type {string}
+   * @memberof MbTopic
+   */
   readonly hashtags?: string
+  /**
+   *
+   *
+   * @type {string}
+   * @memberof MbTopic
+   */
   readonly docList?: string
+  /**
+   *
+   *
+   * @type {DictObj}
+   * @memberof MbTopic
+   */
   readonly options?: DictObj
+  /**
+   *
+   *
+   * @type {Array<any>}
+   * @memberof MbTopic
+   */
   readonly documents?: Array<any>
+  /**
+   *
+   *
+   * @type {Array<any>}
+   * @memberof MbTopic
+   */
   readonly notes?: Array<any>
+  /**
+   *
+   *
+   * @type {boolean}
+   * @memberof MbTopic
+   */
   hideLinksInMindMapNode: boolean
 }
 
+/**
+ * @function getNotebookById
+ * @function getMediaByHash
+ * @function getNoteById
+ * @function getDocumentById
+ * @function getFlashcardByNoteId
+ * @function getFlashcardsByNoteId
+ * @function hasFlashcardByNoteId
+ * @function savedb
+ * @function allNotebooks
+ * @function allDocuments
+ * @function setNotebookSyncDirty
+ * @function saveHistoryArchiveKey
+ * @function loadHistoryArchiveKey
+ * @function deleteBookNote
+ * @function deleteBookNoteTree
+ * @function cloneNotesToTopic
+ * @function cloneNotesToFlashcardsToTopic
+ * @function exportNotebookStorePath
+ * @function importNotebookFromStorePathMerge
+ */
 export class MbModelTool {
   /**
    * not accessible as a static function in global scope, use {@link Database} instead
-   * @returns MbModelTool*
+   * @returns {MbModelTool*}
    */
   // static sharedInstance(): MbModelTool;
   /**
+   * get Notebook by ID
    * @param topicid NSString*
+   * @returns {MbTopic*}
+   * @memberof MbModelTool
+   * @example
+   * ```
+   * Database.sharedInstance().getNotebookById(node.notebookId)
+   * ```
    */
   getNotebookById(topicid: string): WrapperObj<MbTopic> | undefined
   /**
-   * @returns NSData*
+   * @returns {NSData*}
    * @param hash NSString*
+   * @memberof MbModelTool
+   * 
    */
   getMediaByHash(hash: string): NSData | undefined
   /**
    * @param noteid NSString*
+   * @returns {MbBookNote*}
+   * @memberof MbModelTool
    */
   getNoteById(noteid: string): WrapperObj<MbBookNote> | undefined
   /**
    * @param md5 NSString*
+   * @returns {MbBookNote*}
+   * @memberof MbModelTool
    */
   getDocumentById(md5: string): WrapperObj<MbBook> | undefined
   /**
    * @param noteid NSString*
    * @param topicid NSString*
+   * @returns {MbBookNote*}
+   * @memberof MbModelTool
    */
   getFlashcardByNoteId(
     noteid: string,
     topicid: string
   ): WrapperObj<MbBookNote> | undefined
   /**
-   * @returns NSArray*
+   * @returns {NSArray*}
    * @param noteid NSString*
+   * @memberof MbModelTool
+   * 
    */
   getFlashcardsByNoteId(noteid: string): Array<MbBookNote> | undefined
   /**
    * @param noteid NSString*
+   * @returns {boolean}
+   * @memberof MbModelTool
    */
   hasFlashcardByNoteId(noteid: string): boolean
+  /**
+   * @memberof MbModelTool
+   * @returns {void}
+   */
   savedb(): void
   /**
-   * @returns NSArray*
+   * @memberof MbModelTool
+   * @returns {NSArray*}
    */
   allNotebooks(): Array<MbTopic>
   /**
-   * @returns NSArray*
+   * @memberof MbModelTool
+   * @returns {NSArray*}
    */
   allDocuments(): Array<MbBook>
   /**
    * @param topicid NSString*
+   * @returns {void}
+   * @memberof MbModelTool
    */
   setNotebookSyncDirty(topicid: string): void
   /**
-   * @returns NSArray*
+   * @returns {NSArray*}
    * @param topicid NSString*
    * @param key NSString*
+   * @memberof MbModelTool
    */
   saveHistoryArchiveKey(topicid: string, key: string): Array<any>
   /**
-   * @returns NSArray*
+   * @returns {NSArray*}
    * @param topicid NSString*
    * @param key NSString*
+   * @memberof MbModelTool
    */
   loadHistoryArchiveKey(topicid: string, key: string): Array<any>
   /**
    * @param noteid NSString*
+   * @returns {void}
+   * @memberof MbModelTool
    */
   deleteBookNote(noteid: string): void
   /**
    * @param noteid NSString*
+   * @returns {void}
+   * @memberof MbModelTool
    */
   deleteBookNoteTree(noteid: string): void
   /**
-   * @returns NSArray*
+   * @returns {NSArray*}
    * @param notes NSArray*
    * @param topicid NSString*
+   * @memberof MbModelTool
    */
   cloneNotesToTopic(
     notes: Array<MbBookNote>,
     topicid: string
   ): Array<MbBookNote>
   /**
-   * @returns NSArray*
+   * @returns {NSArray*}
    * @param notes NSArray*
    * @param topicid NSString*
+   * @memberof MbModelTool
    */
   cloneNotesToFlashcardsToTopic(
     notes: Array<MbBookNote>,
@@ -117,10 +301,14 @@ export class MbModelTool {
   /**
    * @param topicid NSString*
    * @param storePath NSString*
+   * @returns {boolean}
+   * @memberof MbModelTool
    */
   exportNotebookStorePath(topicid: string, storePath: string): boolean
   /**
    * @param storePath NSString*
+   * @returns {WrapperObj<any>}
+   * @memberof MbModelTool
    */
   importNotebookFromStorePathMerge(
     storePath: string,
@@ -132,7 +320,7 @@ declare global {
   class Database {
     /**
      * accessor to MbModelTool in global scope
-     * @returns MbModelTool*
+     * @returns {MbModelTool*}
      */
     static sharedInstance(): MbModelTool
   }
--- src/typings/MarginNote/Utility.d.ts
+++ src/typings/MarginNote/Utility.d.ts
@@ -1,59 +1,196 @@
 import { UIWindow, UIViewController, UITableView } from "typings/UIKit"
 
 declare global {
+  /**
+   * speech manager
+   * @class SpeechManager
+   * @property {string} speaking
+   * @property {boolean} paused
+   * @property {UIWindow} sceneWindow
+   * @property {string} languageCode
+   * @function startSpeechNotes
+   * @function stopSpeech
+   * @function pauseSpeech
+   * @function continueSpeech
+   * @function prevSpeech
+   * @function nextSpeech
+   * @function canPrev
+   * @function canNext
+   * @function playText
+   * @function playTextLanguageTxt
+   * @function sharedInstance
+   */
   class SpeechManager {
     /**
+     * start speech notes
      * @param notes NSArray*
+     * @memberof SpeechManager
+     * @returns {void}
      */
     startSpeechNotes(notes: Array<any>): void
+    /**
+     * stop speech
+     * @memberof SpeechManager
+     * @returns {void}
+     */
     stopSpeech(): void
+    /**
+     * pause speech
+     * @memberof SpeechManager
+     * @returns {void}
+     */
     pauseSpeech(): void
+    /**
+     * continue speech
+     * @memberof SpeechManager
+     * @returns {void}
+     */
     continueSpeech(): void
+    /**
+     * previous speech
+     * @memberof SpeechManager
+     * @returns {void}
+     */
     prevSpeech(): void
+    /**
+     * next speech
+     * @memberof SpeechManager
+     * @returns {void}
+     */
     nextSpeech(): void
+    /**
+     * @memberof SpeechManager
+     * @returns {boolean}
+     */
     canPrev(): boolean
+    /**
+     * @memberof SpeechManager
+     * @returns {boolean}
+     */
     canNext(): boolean
     /**
      * @param text NSString*
+     * @memberof SpeechManager
+     * @returns {void}
      */
     playText(text: string): void
     /**
      * @param text NSString*
      * @param languageTxt NSString*
+     * @memberof SpeechManager
+     * @returns {void}
      */
     playTextLanguageTxt(text: string, languageTxt: string): void
+    /**
+     * If speaking
+     *
+     * @type {boolean}
+     * @memberof SpeechManager
+     */
     readonly speaking: boolean
+    /**
+     * If paused
+     *
+     * @type {boolean}
+     * @memberof SpeechManager
+     */
     readonly paused: boolean
+    /**
+     * Scene window
+     *
+     * @type {UIWindow}
+     * @memberof SpeechManager
+     */
     sceneWindow?: UIWindow
+    /**
+     * 
+     *
+     * @type {string}
+     * @memberof SpeechManager
+     */
     languageCode?: string
     /**
-     * @returns SpeechManager*
+     * create a new instance
+     * @returns {SpeechManager*}
      */
     static sharedInstance(): SpeechManager
   }
 
+  /**
+   * Undo Manager
+   * @class UndoManager
+   * @function undo
+   * @function redo
+   * @function canUndo
+   * @function canRedo
+   * @function clearAll
+   * @function sharedInstance
+   */
   class UndoManager {
     /**
-     * @returns id<JSBUndoManager>
+     * create a new instance
+     * @returns {UndoManager}
+     * @memberof UndoManager
      */
     static sharedInstance(): UndoManager
     /**
      * @param actionName NSString*
      * @param topicid NSString*
      * @param block JSValue*
+     * @memberof UndoManager
+     * @returns {void}
      */
     undoGrouping(actionName: string, topicid: string, block: JSValue): void
+    /**
+     * undo an action
+     * @memberof UndoManager
+     * @returns {void}
+     */
     undo(): void
+    /**
+     * redo an action
+     * @memberof UndoManager
+     * @returns {void}
+     */
     redo(): void
+    /**
+     * if can undo
+     * @memberof UndoManager
+     * @returns {boolean}
+     */
     canUndo(): boolean
+    /**
+     * if can redo
+     * @memberof UndoManager
+     * @returns {boolean}
+    */
     canRedo(): boolean
+    /**
+     * clear all actions
+     * @memberof UndoManager
+     * @returns {void}
+     */
     clearAll(): void
   }
 
+  /**
+   * zip & unzip tools
+   * @function unzipFileAtPathToDestination
+   * @function unzipFileAtPathToDestinationOverwritePassword
+   * @function createZipFileAtPathWithFilesAtPaths
+   * @function createZipFileAtPathWithContentsOfDirectory
+   * @function initWithPath
+   * @function open
+   * @function writeFile
+   * @function writeDataFilename
+   * @function close
+   */
   class ZipArchive {
     /**
      * @param path NSString*
      * @param destination NSString*
+     * @memberof ZipArchive
+     * @returns {void}
      */
     static unzipFileAtPathToDestination(
       path: string,
@@ -63,6 +200,8 @@ declare global {
      * @param path NSString*
      * @param destination NSString*
      * @param password NSString*
+     * @memberof ZipArchive
+     * @returns {void}
      */
     static unzipFileAtPathToDestinationOverwritePassword(
       path: string,
@@ -73,6 +212,8 @@ declare global {
     /**
      * @param path NSString*
      * @param filenames NSArray*
+     * @memberof ZipArchive
+     * @returns {void}
      */
     static createZipFileAtPathWithFilesAtPaths(
       path: string,
@@ -81,6 +222,8 @@ declare global {
     /**
      * @param path NSString*
      * @param directoryPath NSString*
+     * @memberof ZipArchive
+     * @returns {void}
      */
     static createZipFileAtPathWithContentsOfDirectory(
       path: string,
@@ -88,24 +231,58 @@ declare global {
     ): boolean
     /**
      * @param path NSString*
+     * @memberof ZipArchive
+     * @returns {void}
      */
     initWithPath(path: string): WrapperObj<any>
     open(): boolean
     /**
      * @param path NSString*
+     * @memberof ZipArchive 
+     * @returns {void}
      */
     writeFile(path: string): boolean
     /**
      * @param data NSData*
      * @param filename NSString*
+     * @memberof ZipArchive
+     * @returns {void}
      */
     writeDataFilename(data: NSData, filename: string): boolean
+    /**
+     * @memberof ZipArchive
+     * @returns {boolean}
+     */
     close(): boolean
   }
-
+  /**
+   * @property {UITableView} menuTableView
+   * @function new
+   * @property {DictObj[]} commandTable
+   * @property {Array<any>} sections
+   * @property {number} rowHeight
+   * @property {number} secHeight
+   * @property {number} fontSize
+   * @property {Object} preferredContentSize
+   */
   class MenuController extends UIViewController {
+    /**
+     * new instance
+     * @returns {MenuController}
+     * @memberof MenuController
+     */
     static new(): MenuController
+    /**
+     *
+     *
+     * @type {UITableView}
+     * @memberof MenuController
+     */
     menuTableView?: UITableView
+    /**
+     *
+     * @memberof MenuController
+     */
     commandTable?: {
       title: string
       /** OC Object */
@@ -115,19 +292,43 @@ declare global {
       param: AnyProperty<any>
       checked: boolean
     }[]
+    /**
+     *
+     *
+     * @type {Array<any>}
+     * @memberof MenuController
+     */
     sections?: Array<any>
     /**
      *  int
+     * 
+     * @type {number}
+     * @memberof MenuController
      */
     rowHeight: number
     /**
      *  int
+     * 
+     * @type {number}
+     * @memberof MenuController
      */
     secHeight: number
     /**
      *  int
+     * 
+     * @type {number}
+     * @memberof MenuController
      */
     fontSize: number
+    /**
+     *
+     *
+     * @type {{
+     *       width: number
+     *       height: number
+     *     }}
+     * @memberof MenuController
+     */
     preferredContentSize: {
       width: number
       height: number
