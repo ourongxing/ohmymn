bdbce75d7b262e13b55131b9cb1366b3d9818114
ourongxing <orongxing@gmail.com>
2022-04-20 14:16:25 +0800

    chore: auto format when push

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:prettier/recommended"
   ],
   rules: {
-    "prettier/prettier": "off",
+    // "prettier/prettier": "on",
     "no-unused-vars": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/ban-ts-comment": "off",
