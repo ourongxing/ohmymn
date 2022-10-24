7aa029b5c84d08c86c3d6e698e75794aa562ea78
ourongxing <orongxing@gmail.com>
2022-10-24 11:12:01 +0800

    improve: shortcut pro to custom shortcut

--- src/modules/shortcut/lang.ts
+++ src/modules/shortcut/lang.ts
@@ -6,22 +6,23 @@ export default i18n({
       "【3.7.19】通过 URL Scheme 来触发 MagicAction 中的动作，你可以设置快捷键来打开 URL。该功能完全由 OhMyMN 提供，与 MN 无关。点击查看设置方法及注意事项。",
     shortcut_pro: {
       help: "直接通过 URL Scheme 设置动作的参数，而不需要输入框。可以设置无限多个捷径。",
-      label: "捷径 Pro"
+      label: "自定义捷径"
     },
     card_shortcut: "卡片动作捷径",
     action_not_work: "模块未启用，该动作无法执行",
     text_shortcut: "文字动作捷径",
     shortcut_range: "shortcut 参数请传入范围内的整数",
     no_action: "必须传入 action 参数",
-    action_not_exist: "action 参数不正确",
-    option_interger: "option 参数请传入整数"
+    action_not_exist: "action 不存在",
+    option_interger: "option 参数请传入整数",
+    type_not_exist: "type 参数不正确，必须是 card 或 text"
   },
   en: {
     intro:
       "[3.7.19] Trigger the action in MagicAction through URL Scheme, you can set the shortcut to open the URL. This function is provided by OhMyMN, and has nothing to do with MN. Click to view the setting method and precautions.",
     shortcut_pro: {
       help: "Set the parameters of the action directly through the URL Scheme, without the need for an input box. You can set an unlimited number of shortcuts.",
-      label: "Shortcut Pro"
+      label: "Custom Shortcut"
     },
     card_shortcut: "Card Action Shortcut",
     text_shortcut: "Text Action Shortcut",
@@ -29,7 +30,8 @@ export default i18n({
     shortcut_range:
       "The shortcut parameter should be an integer within the range",
     no_action: "The action parameter must be passed in",
-    action_not_exist: "The action parameter is incorrect",
-    option_interger: "The option parameter should be an integer"
+    action_not_exist: "action does not exist",
+    option_interger: "The option parameter should be an integer",
+    type_not_exist: "The type parameter is incorrect, it must be card or text"
   }
 })
--- src/modules/shortcut/utils.ts
+++ src/modules/shortcut/utils.ts
@@ -11,77 +11,86 @@ export async function handleURLScheme(params: string) {
   try {
     // marginnote3app://addon/ohmymn?type=card&shortcut=1
     // marginnote3app://addon/ohmymn?type=text&shortcut=1
-    // marginnote3app://addon/ohmymn?type=pro&action=renameTitle&option=1&content=1
-    const { type, shortcut, action, option, content } =
+    // marginnote3app://addon/ohmymn?custom=true&type=card&action=renameTitle&option=1&content=1
+    const { custom, type, shortcut, action, option, content } =
       queryString.parse(params)
-    switch (type) {
-      case "card":
-      case "text": {
-        const num = Number(shortcut)
-        if (
-          !Number.isInteger(num) ||
-          (type === "card" && (num < 1 || num > 8)) ||
-          (type === "text" && (num < 1 || num > 4))
-        )
-          throw lang.shortcut_range
-        const opt = self.globalProfile.shortcut[
-          `${type}Shortcut${Number(shortcut) - 1}`
-        ] as [number]
-        const { key, module, option, moduleName } =
-          type === "card" ? actionKey4Card[opt[0]] : actionKey4Text[opt[0]]
-        if (key === "none") return
-        else if (key == "switchPanel") switchPanel()
-        else if (module && !isModuleON(module))
-          showHUD(`${moduleName ?? module} ${lang.action_not_work}`, 2)
-        else {
-          const [sec, row] =
-            dataSourceIndex[
-              type === "card" ? "magicaction4card" : "magicaction4text"
-            ][key]
-          await handleMagicAction(
-            type,
-            self.dataSource[sec].rows[row] as IRowButton,
-            option
-          )
-        }
-        break
-      }
-      case "pro": {
-        const { shortcutPro } = self.globalProfile.shortcut
-        if (!shortcutPro) return
-        if (!action) throw lang.no_action
-        const ret = (() => {
+    if (custom) {
+      const { shortcutPro } = self.globalProfile.shortcut
+      if (!shortcutPro) return
+      if (!action) throw lang.no_action
+      const ret = (() => {
+        if (type === "card") {
           const r = actionKey4Card.find(k => k.key === action)
           if (r)
             return {
               ...r,
               type: "card" as "card" | "text"
             }
+        } else if (type === "text") {
           const k = actionKey4Text.find(k => k.key === action)
           if (k)
             return {
               ...k,
               type: "text" as "card" | "text"
             }
-        })()
-        if (!ret) return lang.action_not_exist
-        const { key, module, moduleName, type } = ret
-        if (module && !isModuleON(module))
-          throw `${moduleName ?? module} ${lang.action_not_work}`
-        else {
-          const [sec, row] =
-            dataSourceIndex[
-              type === "card" ? "magicaction4card" : "magicaction4text"
-            ][key]
-          if (option !== null && !Number.isInteger(Number(option)))
-            throw lang.option_interger
-          const opt = option === null ? undefined : Number(option)
-          await handleMagicAction(
-            type,
-            self.dataSource[sec].rows[row] as IRowButton,
-            opt,
-            content === null ? undefined : String(content)
+        } else {
+          throw lang.type_not_exist
+        }
+      })()
+      if (!ret) throw lang.action_not_exist
+      const { key, module, moduleName, type: _type } = ret
+      if (module && !isModuleON(module))
+        throw `${moduleName ?? module} ${lang.action_not_work}`
+      else {
+        const [sec, row] =
+          dataSourceIndex[
+            _type === "card" ? "magicaction4card" : "magicaction4text"
+          ][key]
+        if (option !== null && !Number.isInteger(Number(option)))
+          throw lang.option_interger
+        const opt = option === null ? undefined : Number(option)
+        await handleMagicAction(
+          _type,
+          self.dataSource[sec].rows[row] as IRowButton,
+          opt,
+          content === null ? undefined : String(content)
+        )
+      }
+    } else {
+      switch (type) {
+        case "card":
+        case "text": {
+          const num = Number(shortcut)
+          if (
+            !Number.isInteger(num) ||
+            (type === "card" && (num < 1 || num > 8)) ||
+            (type === "text" && (num < 1 || num > 4))
           )
+            throw lang.shortcut_range
+          const opt = self.globalProfile.shortcut[
+            `${type}Shortcut${Number(shortcut) - 1}`
+          ] as [number]
+          const { key, module, option, moduleName } =
+            type === "card" ? actionKey4Card[opt[0]] : actionKey4Text[opt[0]]
+          if (key === "none") return
+          else if (key == "switchPanel") switchPanel()
+          else if (module && !isModuleON(module))
+            showHUD(`${moduleName ?? module} ${lang.action_not_work}`, 2)
+          else {
+            const [sec, row] =
+              dataSourceIndex[
+                type === "card" ? "magicaction4card" : "magicaction4text"
+              ][key]
+            await handleMagicAction(
+              type,
+              self.dataSource[sec].rows[row] as IRowButton,
+              option
+            )
+          }
+          break
+        }
+        default: {
+          throw lang.type_not_exist
         }
       }
     }
